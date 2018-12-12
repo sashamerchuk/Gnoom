444e0c9a79d373418be0e6699f2a4404c62a8150
sashamerchuk <sashamerchuk16@gmail.com>
2018-12-13 00:32:59 +0100

    fetch

--- server.js
+++ server.js
@@ -1,19 +1,29 @@
-const express = require('express');
-const multer = require('multer');
-const sharp = require('sharp');
+const express = require("express");
+const multer = require("multer");
+const sharp = require("sharp");
 const app = express();
-const upload = multer({dest:'public/uploads/'});
+const upload = multer({ dest: "public/uploads/" });
+
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
 
 app.listen(3005, function() {
-console.log("Example app listening on port 3005!\n"); });
+  console.log("Example app listening on port 3005!\n");
+});
 
-app.get('/', function(req, res){
+app.get("/", function(req, res) {
   res.send("Hello world");
 });
 
-app.post('/upload', upload.single('picture'), async (req, res) =>{
+app.post("/upload", upload.single("picture"), async (req, res) => {
   await sharp(req.file.path)
-    .webp({quality: 60})
+    .webp({ quality: 60 })
     .toFile(`${req.file.path}.webp`);
-    res.send({source:`/uploads/${req.file.filename}.webp`});
+  res.send({ source: `/uploads/${req.file.filename}.webp` });
 });
--- src/components/Pictures/RowItem.js
+++ src/components/Pictures/RowItem.js
@@ -1,20 +1,38 @@
 import React from "react";
+import Pictures from ".";
 
 export default class RowItem extends React.Component {
   state = {
     src: "",
     size: ""
   };
+  sendToServer(result) {
+    let myHeaders = new Headers();
+    myHeaders.append("Content-Type", "application/x-www-form-urlencoded");
+    fetch("http://localhost:3005/upload", {
+      headers: myHeaders,
+      method: "POST",
+      body: { picture: result }
+    })
+      .then(function(responce) {
+        return responce.json();
+      })
+      .then(function(data) {
+        console.log("hello", data);
+      });
+  }
   componentDidMount() {
     this.setState({ size: (this.props.file.size / 1000).toFixed(2) });
     let reader = new FileReader();
     reader.onload = e => {
       this.setState({ src: e.target.result });
+      this.sendToServer(e.target.result);
     };
     if (this.props.file) {
       reader.readAsDataURL(this.props.file);
     }
   }
+
   render() {
     return (
       <tr className="pictures__item">
@@ -24,7 +42,9 @@ export default class RowItem extends React.Component {
         <td className="dataPict">{this.state.size}Kb</td>
         <td className="dataPict">1123</td>
         <td>
-          <a href={this.props.file}>Download</a>
+          <a className="downLoad" href={this.state.src}>
+            Download
+          </a>
         </td>
       </tr>
     );
